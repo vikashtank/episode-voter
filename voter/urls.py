4c22ca903cad7723d5141372aa093ba5c1ab846f
Vikash Tank <vikashtank@hotmail.co.uk>
2018-01-16 19:29:48 +0000

    add temporary view

---
+++ voter/urls.py
@@ -0,0 +1,10 @@
+"""
+
+"""
+#from django.urls import path
+from django.conf.urls import url, include
+from . import views
+
+urlpatterns = [
+                url("", views.index, name = "index"),
+              ]
--- voter/views.py
+++ voter/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+"""
+views are where you create things to be seen
+"""
 
+from django.shortcuts import render
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    """
+    returns what to show in browser when someone goes to request
+    """
+    return HttpResponse("hello")
